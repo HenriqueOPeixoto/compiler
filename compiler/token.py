b2daa488a61e7c6b05ea5d77ff7f78856f6bdc7d
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-09-15 16:45:21 -0400

    Define to_string() function

--- compiler/token.py
+++ compiler/token.py
@@ -3,3 +3,6 @@ class Token:
     def __init__(self, value, type) -> None:
         self.value = value
         self.type = type
+
+    def to_string(self) -> str:
+        return '{}: {}'.format(self.value, self.type)
