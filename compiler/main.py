63531aaf7147d9b869cf004ee225710772bacac4
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-10-12 18:02:34 -0400

    implement token reading loop

--- compiler/main.py
+++ compiler/main.py
@@ -13,10 +13,23 @@ args = parse_cmd_args()
 
 try:
     input_file = open(args.file, 'r')
+    content = input_file.read()
     
-    lex_scanner.next_token(content=input_file)
+    # lex_scanner.next_token(content=input_file)
+
+    lex = lex_scanner.LexScanner(content)
+    tokens = []
+    while (True):
+        token = lex.next_token()
+
+        if token.type == -1:
+            break
+
+        tokens.append(token)
 
     input_file.close()
+
+    print(tokens)
 
 except FileNotFoundError:
     print('Não foi possível encontrar o arquivo especificado.')
