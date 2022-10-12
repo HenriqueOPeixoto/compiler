f8b120a9e1cf78e6c541b628d7467adc24e69eb9
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-10-12 18:02:20 -0400

    Implement digit token reading

--- compiler/lex_scanner.py
+++ compiler/lex_scanner.py
@@ -1,12 +1,40 @@
-import token
+import my_token
 
-def next_token(content:str) -> 'list[token.Token]':
-    tokens = []
-    
-    for char in content:
-        a = token.Token(char, 0)
-        tokens.append(a)
+EOF = -1
+DIGIT = 0
+NOT_DIGIT = 1
 
-    for elem in tokens:
-        print(elem.to_string())
-    return tokens
+class LexScanner:
+
+    content = ""
+    state = 0
+    pos = 0
+
+    def __init__(self, content) -> None:
+        self.content = content
+
+    def next_token(self) -> my_token.Token:
+        buffer = []
+        while (True):
+            c = self.content[self.pos]
+
+            if self.state == 0:
+                if c.isnumeric():
+                    buffer.append(c)
+                    self.state = DIGIT
+            elif self.state == DIGIT:
+                if c.isnumeric():
+                    buffer.append(c)
+                else:
+                    self.state = NOT_DIGIT
+            elif self.state == NOT_DIGIT:
+                return my_token.Token(int(buffer), DIGIT)
+            
+            self.pos += 1
+
+            print(buffer)
+
+            if self.pos == len(self.content):
+                return my_token.Token("", EOF)
+
+
