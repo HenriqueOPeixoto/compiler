6df74acdb00a253cdb8d61f6bc469029e3372679
Henrique Peixoto <40720893+HenriqueOPeixoto@users.noreply.github.com>
2022-10-17 20:26:45 -0400

    solved state being anything other than 0 after next_token call

--- compiler/lex_scanner.py
+++ compiler/lex_scanner.py
@@ -7,28 +7,28 @@ NOT_DIGIT = 2
 class LexScanner:
 
     content = ""
-    state = 0
     pos = 0
 
     def __init__(self, content) -> None:
         self.content = content
 
     def next_token(self) -> my_token.Token:
+        state = 0
         buffer = []
         while (True):
             c = self.content[self.pos]
 
-            if self.state == 0:
+            if state == 0:
                 if c.isnumeric():
                     buffer.append(c)
-                    self.state = DIGIT
-            elif self.state == DIGIT:
+                    state = DIGIT
+            elif state == DIGIT:
                 if c.isnumeric():
                     buffer.append(c)
                 else:
-                    self.state = NOT_DIGIT
-            elif self.state == NOT_DIGIT:
-                return my_token.Token(int(buffer), DIGIT)
+                    state = NOT_DIGIT
+            elif state == NOT_DIGIT:
+                return my_token.Token(int("".join(buffer)), DIGIT)
             
             self.pos += 1
 
