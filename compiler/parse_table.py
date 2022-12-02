e3d5174a766e008d4ccc855ce7336055b4473d48
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-12-02 17:04:54 -0400

    finish naming indices to parse table

--- compiler/parse_table.py
+++ compiler/parse_table.py
@@ -1,8 +1,61 @@
 # Regras
-T_PROGRAMA = 0
+PROGRAMA = 0
+CORPO = 1
+DC = 2
+MAIS_DC = 3
+DC_V = 4
+TIPO_VAR = 6
+VARIAVEIS = 7
+MAIS_VAR = 8
+COMANDOS = 9
+MAIS_COMANDOS = 10
+COMANDO = 11
+CONDICAO = 12
+RELACAO = 13
+EXPRESSAO = 14
+TERMO = 15
+OP_UN = 16
+FATOR = 17
+OUTROS_TERMOS = 18
+OP_AD = 19
+MAIS_FATORES = 20
+OP_MUL = 21
+PFALSA = 22
 
 # Terminais
-
+T_PROGRAM = 0 # program
+T_DOT = 1 # .
+T_BEGIN = 2
+T_END = 3
+T_EOF = 4 # @, provavelmente não será usado
+T_SEMICOLON = 5
+T_COLON = 6
+T_REAL = 7 # real
+T_INTEGER = 8 # integer
+T_IDENT = 9
+T_COMMA = 10
+T_READ = 11
+T_WRITE = 12
+T_OPEN_PAR = 13 # (
+T_CLOSE_PAR = 14 # )
+T_IF = 15
+T_THEN = 16
+T_CLOSE_BLOCK = 17 # $ SYMBOL
+T_WHILE = 18
+T_DO = 19
+T_EQUAL = 20
+T_UNEQUAL = 21
+T_GREATER_OR_EQUAL = 22
+T_LESS_OR_EQUAL = 23
+T_GREATER = 24
+T_LESS = 25
+T_MINUS = 26 # -
+T_NUMERO_INT = 27 # Qualquer número inteiro
+T_NUMERO_REAL = 28 # Qualquer número real
+T_PLUS = 29 # +
+T_MUL = 30 # *
+T_DIV = 31 # /
+T_ELSE = 32
 
 parse_table = [
 ['program ident <corpo>','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''],
