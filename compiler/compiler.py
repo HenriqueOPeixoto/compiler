c5ab2c09a346c4717ff0f04eadd4b7d886a6489e
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-12-12 22:00:10 -0400

    prepare code outline

---
+++ compiler/compiler.py
@@ -0,0 +1,121 @@
+import my_token
+
+class Compiler:
+
+    def __init__(self, tokens: list[my_token.Token]) -> None:
+        self.tokens = tokens
+        self.tokens.reverse()
+
+    # Geração de código objeto
+    def compile(self):
+        self.code = []
+        self.data = []
+
+        while not 'PARA' in self.code:
+            token_atual = self.tokens[-1]
+            
+            token_atual = self.tokens[-1]
+
+            if token_atual.type == my_token.TokenType.KEYWORD:
+                if token_atual.value == 'program':
+                    self.code.append('INPP')
+                
+                elif token_atual.value == 'begin':
+                    pass
+                
+                elif token_atual.value == 'end':
+                    pass
+
+                elif token_atual.value == 'real':
+                    pass
+                    
+                elif token_atual.value == 'integer':
+                    pass
+                
+                elif token_atual.value == 'read':
+                    pass
+                
+                elif token_atual.value == 'write':
+                    pass
+                
+                elif token_atual.value == 'if':
+                    pass
+                
+                elif token_atual.value == 'then':
+                    pass
+
+                elif token_atual.value == 'while':
+                   pass
+                
+                elif token_atual.value == 'do':
+                   pass
+                
+                elif token_atual.value == 'else':
+                    pass
+
+            elif token_atual.type == my_token.TokenType.IDENT:
+                pass
+
+            elif (token_atual.type == my_token.TokenType.SPACE or
+                token_atual.type == my_token.TokenType.COMMENT):
+                self.tokens.pop()
+            
+            elif token_atual.type == my_token.TokenType.NEWLINE_TOKEN:
+                self.linenum += 1
+                self.tokens.pop()
+            
+            elif token_atual.type == my_token.TokenType.ATRIB:
+               pass
+            
+            elif token_atual.type == my_token.TokenType.SEPARATOR:
+                if token_atual.value == ',':
+                    pass
+
+                elif token_atual.value == ';':
+                    pass
+
+                elif token_atual.value == '$':
+                    pass
+
+                elif token_atual.value == '.':
+                    if self.code[-1] == '.' and len(self.code) == 1:
+                        pass
+                    else:
+                        pass
+
+            elif token_atual.type == my_token.TokenType.OPEN_PAR:
+                if token_atual.value == '(':
+                    pass
+
+            elif token_atual.type == my_token.TokenType.CLOSE_PAR:
+                if token_atual.value == ')':
+                    pass
+            
+            elif token_atual.type == my_token.TokenType.OPERATOR:
+                if token_atual.value == '*':
+                    pass
+                
+                elif token_atual.value == '/':
+                    pass
+                
+                elif token_atual.value == '+':
+                    pass
+                
+                elif token_atual.value == '-':
+                    pass
+            
+            
+            elif token_atual.type == my_token.TokenType.DIGIT:
+                pass
+            
+            elif token_atual.type == my_token.TokenType.REAL_NUM:
+                pass
+
+            elif token_atual.type == my_token.TokenType.LOGICAL_OP:
+                pass
+
+            else:
+                print('stack:', self.code)
+                print('last token:', token_atual.to_string())
+                break
+        
