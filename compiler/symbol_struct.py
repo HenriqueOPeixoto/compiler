9b8a28ce39e9fd490009575d13e5a4e8ef4411f9
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-12-12 21:09:13 -0400

    define fields for every symbol entry on the table

---
+++ compiler/symbol_struct.py
@@ -0,0 +1,7 @@
+# Definição de cada entrada na tabela de símbolos
+
+class SymbolTableData:
+
+    def __init__(self, type, address) -> None:
+        self.type = type
+        self.address = address
